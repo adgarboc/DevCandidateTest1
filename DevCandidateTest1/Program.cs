0fa3ba175a5e23541d3747ec89da3a47fbc43814
Adán García <adgarboc@gmail.com>
2021-07-06 22:05:13 -0500

    Upload project

---
+++ DevCandidateTest1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DevCandidateTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
