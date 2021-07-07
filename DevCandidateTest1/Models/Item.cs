ace868baba1e0d1263b185485a20f5b7cf99fa10
Adán García <adgarboc@gmail.com>
2021-07-06 22:15:06 -0500

    Models created

---
+++ DevCandidateTest1/Models/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevCandidateTest1.Models
+{
+    internal class Item
+    {
+        internal string Nombre { get; set; }
+        internal int Cantidad { get; set; }
+        internal decimal Precio { get; set; }
+    }
+}
---
+++ DevCandidateTest1/Models/ShoppingCart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevCandidateTest1.Models
+{
+    internal class ShoppingCart
+    {
+        public DateTime FechaCompra { get; set; }
+        public Item Items  { get; set; }
+        public decimal TotalCompra { get; set; }
+    }
+}
